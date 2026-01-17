70d60b9574d3c29bd20637f8d7b9b318560bb4c0
nirmal <nirmalailab@gmail.com>
2026-01-17 03:53:37 +0000

    api key

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: 'YOUR_GEMINI_API_KEY_HERE'})],
+  plugins: [googleAI({apiKey: 'AIzaSyC2RCZFUMAMRl1hgXyW3Kar5fDwZ_QSknE'})],
   model: 'googleai/gemini-2.5-flash',
 });
